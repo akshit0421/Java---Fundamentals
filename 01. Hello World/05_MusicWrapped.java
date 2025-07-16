a5fe3c02a50d49bfc5a019eb606641b55db9da3a
akshit0421 <akshit0421@gmail.com>
2025-07-16 21:52:07 +0530

    Music I love

---
+++ 01. Hello World/05_MusicWrapped.java
@@ -0,0 +1,21 @@
+/**
+ * Woah? What's this? 🤔 It's a weird looking comment. Another *, eh?
+ * Write a little bit about yourself here. What's your name? Where are you from? What do you do?
+ *  Hi! My name is Akshit Goyal
+ * I am from India. And I am currently a college student trying to learn coding!
+ * I'm a comment, and I want to say hi! 👋
+ */
+
+public class MusicWrapped {
+    public static void main(String[] args) {
+        System.out.println("========== 🎵 Music Wrapped 🎵 ==========");
+
+        System.out.println("Television/ so Far so Good"); //Apricot Princess
+        System.out.println("Angel Face"); // Stephen Sanchez
+        System.out.println("Say Yes To Heaven"); //Lana Del Rey
+        System.out.println("Fallin' (Adrenaline)"); //Why Don't We
+        System.out.println("Drive"); //Ed Sheeran
+
+        System.out.println("======= 🎵 🎵 🎵 🎵 🎵 🎵 🎵 🎵 ========");
+    }
+}
