f7709e3d2eb21de8d4a19b126c8e3be1ec1ade42
akshit0421 <akshit0421@gmail.com>
2025-07-18 20:30:24 +0530

    new learn

---
+++ 03. Control Flow/01_Decide.java
@@ -0,0 +1,9 @@
+public class Decide {
+    public static void main(String[] args) {
+        int oats = 2;
+
+        if (oats > 0) {
+            System.out.println("Let's make oatmeal today!");
+        }
+    }
+}
