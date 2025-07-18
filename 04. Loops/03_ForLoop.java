830b4b4215a97832a75646d8c2e7c2240d541f7e
akshit0421 <akshit0421@gmail.com>
2025-07-18 22:28:13 +0530

    for

---
+++ 04. Loops/03_ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        for (int i = 1; i <= 50; i++) {
+            System.out.print("I will be great at Programming! 🚀");
+        }
+    }
+}
