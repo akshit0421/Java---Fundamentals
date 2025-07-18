17d7727c43a6f7343c7101a9d2b91c48d229bbce
akshit0421 <akshit0421@gmail.com>
2025-07-18 20:42:33 +0530

    For loop

---
+++ 03. Control Flow/02_DecidePets.java
@@ -0,0 +1,11 @@
+public class DecidePets {
+    public static void main(String[] args) {
+        boolean isCatOwner = true;
+
+        if (isCatOwner == true) {
+            System.out.println("20% off select cat items with code MEOW2025");
+        } else {
+            System.out.println("Welcome to the Pets Pets Pets store!");
+        }
+    }
+}
