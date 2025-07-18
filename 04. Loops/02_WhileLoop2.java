7aa8cb478d2f07bdc16b834f97ef422fd823118e
akshit0421 <akshit0421@gmail.com>
2025-07-18 22:23:25 +0530

    While loop

---
+++ 04. Loops/02_WhileLoop2.java
@@ -0,0 +1,9 @@
+public class WhileLoop2 {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 15) {
+            System.out.println("Line " + num + ": Let's Go " + num);
+            num += 1;
+        }
+    }
+}
