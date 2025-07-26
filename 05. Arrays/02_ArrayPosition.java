dd0f82e5b9bd15959bb599c7835c8d30b5999d85
akshit0421 <akshit0421@gmail.com>
2025-07-26 10:16:30 +0530

    p2

---
+++ 05. Arrays/02_ArrayPosition.java
@@ -0,0 +1,9 @@
+public class arrayPosition {
+    public static void main(String[] args) {
+        //Access the array
+        String[] sodaDrinks = {"sprite", "fanta","thumb up", "pepsi", "coca-cola","lahori jera", "redbull","monster"};
+        System.out.println(sodaDrinks[3]);
+        System.out.println(sodaDrinks[4]);
+        System.out.println(sodaDrinks[7]);
+    }
+}
