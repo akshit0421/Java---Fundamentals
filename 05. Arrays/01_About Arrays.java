f735cf8909995558f664279e100f6f017058f474
akshit0421 <akshit0421@gmail.com>
2025-07-26 10:13:30 +0530

    p1

---
+++ 05. Arrays/01_About Arrays.java
@@ -0,0 +1,9 @@
+public class AboutArrays {
+    public static void main(String[] args) {
+        //Define an array with elements
+        String[] zombieSurvivalPack = {"FlameThrower", "ShotGun", "Medical Kit", "Water", "Food", "Dagger", "Helmet", "Army Waist"};
+        //Define an array without elements
+        int[] emptyArray = new int[5];
+        double[] grades = {83.4, 77.9, 62.8, 99.8};
+    }
+}
